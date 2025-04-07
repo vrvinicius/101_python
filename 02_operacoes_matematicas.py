559085b2c790cb2eb85a9a9ca6463a69ee928aa3
viniciusvieira <viniciusrvieira@outlook.com.br>
2025-04-06 22:02:47 -0300

    first commit

---
+++ 02_operacoes_matematicas.py
@@ -0,0 +1,21 @@
+# %%
+# soma
+print("1 + 1 = ", 1 + 1)
+
+# subtração
+print("10 - 5 = ", 1 - 1)
+
+# multiplicação
+print("2 x 3 = ", 2 * 3)
+
+# divisão
+print("10 / 2 = ", 10 / 2)
+
+# divisão inteira
+print("10 // 3 = ", 10 // 3)
+
+# resto da divisão
+print("10 % 4 = ", 10 % 4)
+
+# potenciação
+print("2 ** 3 = ", 2 ** 3)
---
+++ 03_variaveis.py
@@ -0,0 +1,17 @@
+# %%
+nome = "Vinícius Vieira"
+print(nome)
+
+nome = "Lenovo Gaming"
+print(nome)
+
+# %%
+soma = 1 + 1
+print("1 + 1 =", soma)
+
+# %%
+resultado = 10 * 2
+print(resultado)
+
+resultado = resultado + 10
+print(resultado)
